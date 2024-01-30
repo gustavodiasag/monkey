4947ebd6fe612c2d50346f8a2c3cc78e07b53392
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-01-29 22:17:47 -0300

    test(ast) include test suite for ast nodes

---
+++ ast/ast_test.go
@@ -0,0 +1,28 @@
+package ast
+
+import (
+    "monkey/monkey"
+    "testing"
+)
+
+func TestString(t *testing.T) {
+    program := &Program{
+        Statements: []Statement{
+            &LetStatement{
+                Token: token.Token{Type: token.IDENT, Literal: "let"},
+                Name: &Identifier{
+                    Token: token.Token{Type: token.IDENT, Literal: "var"}.
+                    Value: "var",
+                },
+                Value: &Identifier{
+                    Token: token.Token{Type: token.IDENT, Literal: "foo"},
+                    Value: "foo",
+                },
+            },
+        },
+    }
+
+    if program.String() !=  "let var = foo;" {
+        t.Errorf("program.String() wrong. got: %q", program.String())
+    }
+}
