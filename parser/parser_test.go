ae42550ebd207839ada1c152ad74fa70442dd5d2
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-01-29 22:21:10 -0300

    test(parser): identifier expression

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,7 +17,8 @@ let foobar = 1000;
 
 	program := p.ParseProgram()
     checkParserErrors(t, p)
-	if program == nil {
+	
+    if program == nil {
 		t.Fatalf("ParseProgram() returned 'nil'")
 	}
 	if len(program.Statements) != 3 {
@@ -36,6 +37,63 @@ let foobar = 1000;
 			return
 		}
 	}
+}
+
+func TestReturnStatement(t *testing.T) {
+    input := `
+return 0;
+return 10;
+return 1235135;
+`
+    l := lexer.New(input)
+    p := New(l)
+    
+    program := p.ParseProgram()
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 3 {
+        t.Fatalf("program.Statements not 3. got %d", len(program.Statements))
+    }
+
+    for _, stmt := range program.Statements {
+        returnStmt, ok := stmt.(*ast.ReturnStatement)
+        if !ok {
+            t.Errorf("stmt not *ast.ReturnStatement. got %T", stmt)
+            continue
+        }
+        if returnStmt.TokenLiteral() != "return" {
+            t.Errorf("returnStmt.TokenLiteral not 'return'. got %q", returnStmt.TokenLiteral())
+        }
+    }
+}
+
+func TestIdentifierExpression(t *testing.T) {
+    input := "foobar;"
+
+    l := lexer.New(input)
+    p := New(l)
+    program := p.ParseProgram()
+    checkParserErrors(t, p)
+
+    if len(program.Statements) != 1 {
+        t.Fatalf("program.Statements not 1. got %d", len(program.Statements))
+    }
+    stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+    if !ok {
+        t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got %T",
+            program.Statements[0])
+    }
+
+    ident, ok := stmt.Expression.(*ast.Identifier)
+    if !ok {
+        t.Fatalf("exp not *ast.Identifier. got %T", stmt.Expression)
+    }
+    if ident.Value != "foobar" {
+        t.Errorf("ident.Value not %s. got %s", "foobar", ident.Value)
+    }
+    if ident.TokenLiteral() != "foobar" {
+        t.Errorf("ident.TokenLiteral not %s. got %s", "foobar", ident.TokenLiteral())
+    }
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
