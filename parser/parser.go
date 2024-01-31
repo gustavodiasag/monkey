3b11bf771a274930c29ae111d3772ca0903cc9d3
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-01-31 18:56:46 -0300

    feat(parser): include parsing rules for call expressions

--- parser/parser.go
+++ parser/parser.go
@@ -45,6 +45,7 @@ var precedences = map[token.TokenType]int{
 	token.MINUS: TERM,
 	token.SLASH: FACTOR,
 	token.STAR:  FACTOR,
+    token.LPAREN: CALL,
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -76,6 +77,7 @@ func New(l *lexer.Lexer) *Parser {
 	p.registerInfix(token.NEQ, p.parseInfixExpression)
 	p.registerInfix(token.LT, p.parseInfixExpression)
 	p.registerInfix(token.GT, p.parseInfixExpression)
+    p.registerInfix(token.LPAREN, p.parseCallExpression)
 
 	return p
 }
@@ -322,6 +324,35 @@ func (p *Parser) parseFunctionParameters() []*ast.Identifier {
         return nil
     }
     return identifiers
+}
+
+func (p *Parser) parseCallExpression(function ast.Expression) ast.Expression {
+    exp := &ast.CallExpression{Token: p.curToken, Function: function}
+    exp.Arguments = p.parseCallArguments()
+
+    return exp
+}
+
+func (p *Parser) parseCallArguments() []ast.Expression {
+    args := []ast.Expression{}
+
+    if p.peekTokenIs(token.RPAREN) {
+        p.nextToken()
+        return args
+    }
+
+    p.nextToken()
+    args = append(args, p.parseExpression(LOWEST))
+
+    for p.peekTokenIs(token.COMMA) {
+        p.nextToken()
+        p.nextToken()
+        args = append(args, p.parseExpression(LOWEST))
+    }
+    if !p.expectPeek(token.RPAREN) {
+        return nil
+    }
+    return args
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
