54ec99e5f58f8e30507fae98e2b82c64eb712cd7
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-01 15:41:21 -0300

    feat(object): provide internal value representation for primitive types

---
+++ object/object.go
@@ -0,0 +1,37 @@
+package object
+
+import (
+    "fmt"
+)
+
+type ObjectType string
+
+const (
+    INT_OBJ = "INTEGER"
+    BOOL_OBJ = "BOOLEAN"
+    NULL_OBJ = "NULL"
+)
+
+type Object interface {
+    Type() ObjectType
+    Inspect() string
+}
+
+type Integer struct {
+    Value int64
+}
+
+func (i *Integer) Type() ObjectType { return INT_OBJ }
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) } 
+
+type Boolean struct {
+    Value bool
+}
+
+func (b *Boolean) Type() ObjectType { return BOOL_OBJ }
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+type Null struct{}
+
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+func (n *Null) Inspect() string { return "null" }
