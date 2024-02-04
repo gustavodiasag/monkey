f08b248fdba507359ce5fcd3421bc2d9c853b742
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-04 19:53:27 -0300

    feat(eval): provide evaluation of infix expressions

--- eval/eval.go
+++ eval/eval.go
@@ -1,79 +1,120 @@
 package eval
 
 import (
-    "monkey/ast"
-    "monkey/object"
+	"monkey/ast"
+	"monkey/object"
 )
 
 var (
-    TRUE = &object.Boolean{Value: true}
-    FALSE = &object.Boolean{Value: false}
-    NULL = &object.Null{}
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
+	NULL  = &object.Null{}
 )
 
 func Eval(node ast.Node) object.Object {
-    switch node := node.(type) {
-    case *ast.Program:
-        return evalStatements(node.Statements)
-    case *ast.ExpressionStatement:
-        return Eval(node.Expression)
-    case *ast.IntegerLiteral:
-        return &object.Integer{Value: node.Value}
-    case *ast.Boolean:
-        return booleanObject(node.Value)
-    case *ast.PrefixExpression:
-        right := Eval(node.Right)
-        return evalPrefixExpression(node.Operator, right)
-    }
-    
-    return nil
+	switch node := node.(type) {
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return booleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+	case *ast.InfixExpression:
+		left := Eval(node.Left)
+		right := Eval(node.Right)
+		return evalInfixExpression(node.Operator, left, right)
+	}
+
+	return nil
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
-    var result object.Object
+	var result object.Object
 
-    for _, stmt := range stmts {
-        result = Eval(stmt)
-    }
-    return result
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
+	return result
 }
 
 func booleanObject(input bool) *object.Boolean {
-    if input {
-        return TRUE
-    }
-    return FALSE
+	if input {
+		return TRUE
+	}
+	return FALSE
 }
 
 func evalPrefixExpression(operator string, right object.Object) object.Object {
-    switch operator {
-    case "!":
-        return evalBangPrefixOperator(right)
-    case "-":
-        return evalMinusPrefixOperator(right)
-    default:
-        return NULL
-    }
+	switch operator {
+	case "!":
+		return evalBangPrefixOperator(right)
+	case "-":
+		return evalMinusPrefixOperator(right)
+	default:
+		return NULL
+	}
 }
 
 func evalBangPrefixOperator(right object.Object) object.Object {
-    switch right {
-    case TRUE:
-        return FALSE
-    case FALSE:
-        return TRUE
-    case NULL:
-        return TRUE
-    default:
-        return FALSE
-    }
+	switch right {
+	case TRUE:
+		return FALSE
+	case FALSE:
+		return TRUE
+	case NULL:
+		return TRUE
+	default:
+		return FALSE
+	}
 }
 
 func evalMinusPrefixOperator(right object.Object) object.Object {
-    if right.Type() != object.INT_OBJ {
+	if right.Type() != object.INT_OBJ {
+		return NULL
+	}
+	value := right.(*object.Integer).Value
+
+	return &object.Integer{Value: -value}
+}
+
+func evalInfixExpression(
+	operator string,
+	left object.Object,
+	right object.Object,
+) object.Object {
+
+    switch {
+    case left.Type() == object.INT_OBJ && right.Type() == object.INT_OBJ:
+        return evalIntegerInfixExpression(operator, left, right)
+    default:
         return NULL
     }
-    value := right.(*object.Integer).Value
+}
 
-    return &object.Integer{Value: -value}
+func evalIntegerInfixExpression(
+    operator string,
+    left object.Object,
+    right object.Object,
+) object.Object {
+
+    leftVal := left.(*object.Integer).Value
+    rightVal := right.(*object.Integer).Value
+
+    switch operator {
+    case "+":
+        return &object.Integer{Value: leftVal + rightVal}
+    case "-":
+        return &object.Integer{Value: leftVal - rightVal}
+    case "*":
+        return &object.Integer{Value: leftVal * rightVal}
+    case "/":
+        return &object.Integer{Value: leftVal / rightVal}
+    default:
+        return NULL
+    }
 }
