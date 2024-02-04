4289b3125fe3f07988eb9d011309ce02537073ca
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-04 19:19:31 -0300

    refac(eval): create boolean objects with references

--- eval/eval.go
+++ eval/eval.go
@@ -5,6 +5,11 @@ import (
     "monkey/object"
 )
 
+var (
+    TRUE = &object.Boolean{Value: true}
+    FALSE = &object.Boolean{Value: false}
+)
+
 func Eval(node ast.Node) object.Object {
     switch node := node.(type) {
     case *ast.Program:
@@ -14,7 +19,7 @@ func Eval(node ast.Node) object.Object {
     case *ast.IntegerLiteral:
         return &object.Integer{Value: node.Value}
     case *ast.Boolean:
-        return &object.Boolean{Value: node.Value}
+        return booleanObject(node.Value)
     }
     
     return nil
@@ -27,4 +32,11 @@ func evalStatements(stmts []ast.Statement) object.Object {
         result = Eval(stmt)
     }
     return result
+}
+
+func booleanObject(input bool) *object.Boolean {
+    if input {
+        return TRUE
+    }
+    return FALSE
 }
