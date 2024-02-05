face507d378c70c9f836459debef6d59604e23d5
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-05 18:10:59 -0300

    feat(object): define representation for return statements

--- object/object.go
+++ object/object.go
@@ -1,37 +1,45 @@
 package object
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type ObjectType string
 
 const (
-    INT_OBJ = "INTEGER"
-    BOOL_OBJ = "BOOLEAN"
-    NULL_OBJ = "NULL"
+	INT_OBJ  = "INTEGER"
+	BOOL_OBJ = "BOOLEAN"
+	NULL_OBJ = "NULL"
+    RETURN_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
-    Type() ObjectType
-    Inspect() string
+	Type() ObjectType
+	Inspect() string
 }
 
 type Integer struct {
-    Value int64
+	Value int64
 }
 
 func (i *Integer) Type() ObjectType { return INT_OBJ }
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) } 
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 
 type Boolean struct {
-    Value bool
+	Value bool
 }
 
 func (b *Boolean) Type() ObjectType { return BOOL_OBJ }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 
 type Null struct{}
 
 func (n *Null) Type() ObjectType { return NULL_OBJ }
-func (n *Null) Inspect() string { return "null" }
+func (n *Null) Inspect() string  { return "null" }
+
+type ReturnValue struct {
+    Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_OBJ }
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
