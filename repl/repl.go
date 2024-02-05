ced7f01ca3f8a94bfc75860a0bacff6647e1afab
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-05 18:10:31 -0300

    style: formatting

--- repl/repl.go
+++ repl/repl.go
@@ -5,8 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
-    
-    "monkey/eval"
+
+	"monkey/eval"
 	"monkey/lexer"
 	"monkey/parser"
 )
@@ -24,25 +24,25 @@ func Start(in io.Reader, out io.Writer) {
 		}
 		line := scanner.Text()
 		l := lexer.New(line)
-        p := parser.New(l)
+		p := parser.New(l)
 
-        program := p.ParseProgram()
-        if len(p.Errors()) != 0 {
-            printParserErrors(p.Errors())
-            continue
-        }
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(p.Errors())
+			continue
+		}
 
-        evaluated := eval.Eval(program)
-        if evaluated != nil {
-            io.WriteString(out, evaluated.Inspect())
-            io.WriteString(out, "\n")
-        }
-    }
+		evaluated := eval.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
+	}
 }
 
 func printParserErrors(errors []string) {
-    io.WriteString(os.Stderr, " Parser errors:\n")
-    for _, msg := range errors {
-        io.WriteString(os.Stderr, "\t" + msg + "\n")
-    }
+	io.WriteString(os.Stderr, " Parser errors:\n")
+	for _, msg := range errors {
+		io.WriteString(os.Stderr, "\t"+msg+"\n")
+	}
 }
