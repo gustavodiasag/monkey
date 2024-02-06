e4ab26fd5a03d96bb10f3f7fb2ee3726dd0e1704
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-06 14:32:36 -0300

    feat(object): provide representation for function literals

--- object/object.go
+++ object/object.go
@@ -1,7 +1,11 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"strings"
+
+	"monkey/ast"
 )
 
 type ObjectType string
@@ -12,6 +16,7 @@ const (
 	NULL_OBJ   = "NULL"
 	RETURN_OBJ = "RETURN_VALUE"
 	ERROR_OBJ  = "ERROR"
+    FUNCTION_OBJ = "FUNCTION"
 )
 
 type Object interface {
@@ -45,9 +50,35 @@ type ReturnValue struct {
 func (rv *ReturnValue) Type() ObjectType { return RETURN_OBJ }
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 
+type Function struct {
+    Parameters []*ast.Identifier
+    Body *ast.BlockStatement
+    Env *Environment
+}
+
+func (f *Function) Type() ObjectType { return FUNCTION_OBJ }
+func (f *Function) Inspect() string {
+    var out bytes.Buffer
+    
+    params := []string{}
+    for _, param := range f.Parameters {
+        params = append(params, param.String())
+    }
+
+    out.WriteString("fn")
+    out.WriteString("(")
+    out.WriteString(strings.Join(params, ", "))
+    out.WriteString(") {\n")    
+    out.WriteString(f.Body.String())
+    out.WriteString("\n}")
+    
+    return out.String()
+}
+
 type Error struct {
 	Message string
 }
 
 func (e *Error) Type() ObjectType { return ERROR_OBJ }
 func (e *Error) Inspect() string  { return "Error: " + e.Message }
+
