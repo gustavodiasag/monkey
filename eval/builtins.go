899b130ae8403c5435f068644ce6aeb32ca7378d
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-07 16:18:12 -0300

    feat(eval): provide runtime representation of native functions

---
+++ eval/builtins.go
@@ -0,0 +1,20 @@
+package eval
+
+import "monkey/object"
+
+var builtins = map[string]*object.Builtin{
+	"len": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. expected 1")
+			}
+
+			switch arg := args[0].(type) {
+			case *object.String:
+				return &object.Integer{Value: int64(len(arg.Value))}
+			default:
+				return newError("argument to `len` not supported")
+			}
+		},
+	},
+}
