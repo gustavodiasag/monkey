d0dbbb50ebabb59b34de000b71d3476251a973ea
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-14 15:27:29 -0300

    test(eval): add suite for hash literals

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -365,6 +365,47 @@ func TestArrayIndexExpressions(t *testing.T) {
 	}
 }
 
+func TestHashLiterals(t *testing.T) {
+	input := `
+let two = "two";
+
+{
+    "one": 10 - 9,
+    two: 1 + 1,
+    "thr" + "ee": 6 / 2,
+    4: 4,
+    true: 5,
+    false: 6
+}
+`
+	evaluated := testEval(input)
+	result, ok := evaluated.(*object.Hash)
+	if !ok {
+		t.Fatalf("Object not Hash. Got %T (%+v)", evaluated, evaluated)
+	}
+
+	expected := map[object.HashKey]int64{
+		(&object.String{Value: "one"}).HashKey():   1,
+		(&object.String{Value: "two"}).HashKey():   2,
+		(&object.String{Value: "three"}).HashKey(): 3,
+		(&object.Integer{Value: 4}).HashKey():      4,
+		TRUE.HashKey():                             5,
+		FALSE.HashKey():                            6,
+	}
+
+	if len(result.Pairs) != len(expected) {
+		t.Fatalf("Hash has wrong number of pairs. Got %d", len(result.Pairs))
+	}
+
+	for k, v := range expected {
+		pair, ok := result.Pairs[k]
+		if !ok {
+			t.Errorf("No pair for given key in Pairs")
+		}
+		testIntegerObject(t, pair.Value, v)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
