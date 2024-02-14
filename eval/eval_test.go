b4ea450cf913f123c0e5a910a756c181522ac76c
gustavodiasag <gustavodias.aguiar1@gmail.com>
2024-02-14 15:50:00 -0300

    test(eval): add suite for hash map index expressions

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -406,6 +406,50 @@ let two = "two";
 	}
 }
 
+func TestHashIndexExpressions(t *testing.T) {
+    for _, tt := range []struct {
+        input string
+        expected interface{}
+    }{
+        {
+            `{"foo": 5}["foo"]`,
+            5,
+        },
+        {
+            `{"foo": 5}["bar"]`,
+            nil,
+        },
+        {
+            `let key = "foo"; {"foo": 5}[key]`,
+            5,
+        },
+        {
+            `{}["foo"]`,
+            nil,
+        },
+        {
+            `{5: 5}[5]`,
+            5,
+        },
+        {
+            `{true: 5}[true]`,
+            5,
+        },
+        {
+            `{false: 5}[false]`,
+            5,
+        },
+    } {
+        evaluated := testEval(tt.input)
+        integer, ok := tt.expected.(int)
+        if ok {
+            testIntegerObject(t, evaluated, int64(integer))
+        } else {
+            testNullObject(t, evaluated)
+        }
+    }
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
